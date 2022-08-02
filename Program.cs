4b3a902e94a7cd797055b1f51df0f601b3607932
De <spark49942@gmail.com>
2022-08-03 00:21:03 +0300

    DZ1

--- Program.cs
+++ Program.cs
@@ -1,63 +1,28 @@
-﻿void Z() 
+﻿void DZ1() 
 {
-
-
-
+Console.WriteLine("Введите два числа:");
+int A = Convert.ToInt32(Console.ReadLine()), 
+    B = Convert.ToInt32(Console.ReadLine());
+double Ab = Math.Pow(A, B);
+Console.WriteLine(A + " в степени " + B + " = " + Ab);
 }
-Z();
+DZ1();
 
-void Z1() 
+void DZ2() 
 {
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-while (num>0) 
-{
-num = num/10;
-count++;
-}
-Console.WriteLine(count);
 
 
-}
-Z1();
 
-void Z2() 
-{
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine()),
-inum = 0;
-int anum = 1, bnum = 2;
-while (inum<num-1) 
-{
-int res = anum * bnum;
-Console.WriteLine(anum + " * " + bnum + " = " + res);
-anum = anum*bnum;
-bnum++;
-inum++;
-}
 }
 Console.WriteLine("");
-Z2();
+DZ2();
 
 
 Console.WriteLine("");
-void Z3() 
+void DZ3() 
 {
 
-int [] array = new int [8];
-int i = 0;
-Console.Write("[ ");
-while (i<array.Length) 
-{
-int num = new Random().Next(0, 2);
-array [i] = num;
-Console.Write(num + ", ");
-i++;
-}
-Console.Write("\b\b ");
-Console.Write(" ]");
 
 
 }
-Z3();
+DZ3();
