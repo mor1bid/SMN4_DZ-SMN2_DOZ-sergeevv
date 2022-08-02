60409bef399036a1301f60d61c77039943be8d40
De <spark49942@gmail.com>
2022-08-02 21:16:45 +0300

    Z2-2

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine()), 
 inum = 0;
 int anum = 1, res = 2;
-while (inum<num-1) 
+while (inum<num) 
 {
 res = anum * res;
 //Console.WriteLine(anum + " * " + res + " = " + res);
@@ -44,7 +44,7 @@ void Z3()
 
 int [] array = new int [8];
 int i = 0;
-while (i<=array.Length) 
+while (i<array.Length) 
 {
 int num = new Random().Next(0, 1);
 array [i] = num;
