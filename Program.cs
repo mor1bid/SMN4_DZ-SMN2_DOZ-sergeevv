fe40b4bf2a31ead45f8908fe5dc14d36f1b1dd4e
De <spark49942@gmail.com>
2022-08-02 20:44:18 +0300

    Z1

--- Program.cs
+++ Program.cs
@@ -8,7 +8,15 @@ Z();
 
 void Z1() 
 {
-
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (num>0) 
+{
+num = num/10;
+count++;
+}
+Console.WriteLine(count);
 
 
 }
