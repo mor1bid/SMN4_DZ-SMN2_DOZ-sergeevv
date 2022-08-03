f7a0e8a38e21d75b7af6597aa324927a4ee9b7af
De <spark49942@gmail.com>
2022-08-03 04:32:27 +0300

    DZ2-6

--- Program.cs
+++ Program.cs
@@ -16,11 +16,11 @@ Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 //int num = new Random().Next(1, 2000000001);
 int dig, sum = 0, dug = num, dog = num;
-while (dug >= 10) 
+while (dug > 10) 
 {
 dig = dug % 10;
 //dog /= 10;
-dug = (dug % 100) / 10;
+dug /= 10;
 sum = dig + dug;
 }
 Console.WriteLine(num + " --> " + sum);
