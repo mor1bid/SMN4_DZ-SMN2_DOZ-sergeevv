4e51012c59f95e44e9fd400b5b4e2253efd6002b
De <spark49942@gmail.com>
2022-08-03 05:22:56 +0300

    DZ2-8

--- Program.cs
+++ Program.cs
@@ -15,15 +15,16 @@ void DZ2()
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 //int num = new Random().Next(1, 2000000001);
+num = Math.Abs(num);
 int dig, sum = 0, dug = num, dog = num, ten = 10;
-while (dog > 0) 
+while (dug > 0) 
 {
-dog /= ten;
+//dog /= ten;
 dig = dug % 10;
-dug = (dug % 100) / 10;
-dog /= ten;
-ten *= 10; 
-Console.WriteLine(num + "-->" + $"{sum = dig + dug + dog}");
+dug = (dug / 10) / 10;
+//dog /= ten;
+//ten *= 10; 
+Console.WriteLine(num + "-->" + $"{sum = dig + dug}");
 }
 //Console.WriteLine(num + " --> " + sum);
 }
