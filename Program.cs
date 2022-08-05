3701430617fe8992cec4142a367f2a78d7035c0a
De <spark49942@gmail.com>
2022-08-05 04:47:36 +0300

    SMN4+SMN2_DOZ

--- Program.cs
+++ Program.cs
@@ -108,4 +108,119 @@ while (i<co)
 }
 Console.WriteLine("Сумма цифер числа " + num + " = " + sum);
 }
+Console.WriteLine();
 DZ02();
+
+Console.WriteLine();
+void SMN2_DOZ1() 
+{
+    // Задача 1. Написать программу, которая определяет, 
+    // является ли треугольник со сторонами a, b, c равнобедренным.
+
+    Console.WriteLine("1. Введите значения сторон треугольника: ");
+    int a = Convert.ToInt32(Console.ReadLine()), 
+        b = Convert.ToInt32(Console.ReadLine()), 
+        c = Convert.ToInt32(Console.ReadLine());
+if (a == b || c == b || a == c) 
+{
+Console.WriteLine("Данный треугольник abc - равнобедренный");
+}
+else
+Console.WriteLine("Данный треугольник abc - неравнобедренный");
+}
+Console.WriteLine();
+SMN2_DOZ1();
+
+Console.WriteLine();
+void SMN2_DOZ2() 
+{
+    // Задача 2. На вход подаются год, номер месяца и день рождения человека, 
+    // Определить возраст человека на момент 1 июля 2022 года.
+    
+int day = 1, month = 7, year = 2022;
+    Console.WriteLine("2. Введите день, месяц и год вашего рождения: ");
+    int myday = Convert.ToInt32(Console.ReadLine()), 
+        mymonth = Convert.ToInt32(Console.ReadLine()),
+        myyear = Convert.ToInt32(Console.ReadLine());
+    if (myday <= 31 && myday > 0 && mymonth <= 12 && mymonth > 0 && myyear <= year && mymonth > month) 
+    {
+    myday = myday-day;
+    mymonth = mymonth-month;
+    myyear = year-myyear-1;
+    Console.WriteLine("Вам " + myday + " дней, " 
+    + mymonth + " месяцев, " + myyear + " лет.");
+    } 
+    else if (mymonth <= month) 
+    {
+        mymonth = month-mymonth;
+        myyear = year-myyear;
+        Console.WriteLine("Вам " + myday + " дней, " 
+        + mymonth + " месяцев, " + myyear + " лет."); 
+    } 
+    else 
+    {
+        Console.WriteLine("Введена некорректная дата"); 
+    }
+}
+Console.WriteLine();
+SMN2_DOZ2();
+
+Console.WriteLine();
+void SMN2_DOZ3() 
+{
+    // Задача 3. Иван в начале года открыл счет в банке, вложив 1000 руб. 
+    // Через каждый месяц размер вклада увеличивается на 1.5% от имеющейся суммы. 
+    // Определить размер депозита через n месяцев.
+
+double money = 1000, grow = 1.5, month=0;
+    Console.WriteLine("3. Введите кол-во месяцев: ");
+    double mymonth = Convert.ToInt32(Console.ReadLine());
+    while (month<=mymonth) 
+    {
+        if (month % 2 == 0) 
+        {
+            double moneygrow = money * grow / 100; //Чтобы получать полный ответ
+            money += moneygrow;
+        }
+        month++;
+    }
+    Console.WriteLine("Размер депозита через " + mymonth + " месяцев будет равен " + money);
+}
+Console.WriteLine();
+SMN2_DOZ3();
+
+Console.WriteLine();
+void SMN2_DOZ4() 
+{
+    // Дано натуральное число, в котором все цифры различны. 
+    // Определить, какая цифра расположена в нем левее: максимальная или минимальная.
+
+  Console.WriteLine("4. Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine()),
+        min = num % 10,
+        max = 0,
+        dig = num,
+        dug = num,
+        numco=0,
+        namco=0;
+        while (dug > 0)
+        {
+            dig = dug % 10;
+            dug /=10;
+            if (dig>max) 
+            { 
+                max = dig; 
+                numco++; 
+            }
+            else if (dig<=min) 
+            {
+                min = dig; 
+                namco++;
+            }
+        } 
+        Console.WriteLine("В числе " + num + " цифра " + max + " - максимальное, " + min + " - минимальное");
+        if (numco<=namco) Console.WriteLine("Минимальное число находится левее");
+        else Console.WriteLine("Максимальное число находится левее");
+}
+Console.WriteLine();
+SMN2_DOZ4();
